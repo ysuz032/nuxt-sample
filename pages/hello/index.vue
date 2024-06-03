6bfba540daf1acdef56a05dc9656d3a1f2905852
ysuz032 <102312063+ysuz032@users.noreply.github.com.>
2024-06-03 13:16:06 +0000

    1st commit

---
+++ pages/hello/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Hello World</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorldPage'
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: #2c3e50;
+        text-align: center;
+    }
+</style>
---
+++ pages/image/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>image</h1>
+        <img src="https://images.unsplash.com/photo-1716325095357-cc54d2229f05?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img src="https://images.unsplash.com/photo-1715630914955-d91c67c20c2d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img src="https://images.unsplash.com/photo-1716916959342-03078e60b9a6?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img src="https://images.unsplash.com/photo-1715313676060-43e777f5ed48?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img src="https://images.unsplash.com/photo-1717157746019-57940e47648b?q=80&w=1896&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ImagePage'
+    }
+</script>
+
+
+<style scoped>
+    h1 {
+        color: #2c3e50;
+        text-align: center;
+    }
+    img {
+        width: 240pt;
+        height: auto;
+    }
+</style>
